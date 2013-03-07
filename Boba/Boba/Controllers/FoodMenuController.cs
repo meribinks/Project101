bb10916b2b44fcf17e81f7dc5d374977a234029b
M Besmonte <mariabesmonte@gmail.com>
2013-03-07 09:53:40 -0800

    1stcommit

---
+++ Boba/Boba/Controllers/FoodMenuController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Boba.Controllers
+{
+    public class FoodMenuController : Controller
+    {
+        //
+        // GET: /FoodMenu/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
